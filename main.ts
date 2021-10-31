fcf3308353e8d683c0f66500484f9c68c5ac9542
Carson <85135220+ProgrammingGeek2@users.noreply.github.com>
2021-10-31 16:40:27 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,68 @@
+namespace SpriteKind {
+    export const Gas = SpriteKind.create()
+}
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(assets.image`galgaDart2`, mySprite, 0, -50)
+    projectile.startEffect(effects.fire)
+})
+info.onLifeZero(function () {
+    game.over(false)
+    mySprite.destroy(effects.fire, 500)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Gas, function (sprite, otherSprite) {
+    statusbar.value = 100
+    myFuel.destroy()
+})
+statusbars.onZero(StatusBarKind.Energy, function (status) {
+    game.over(false)
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    myEnemy.destroy(effects.disintegrate, 500)
+    projectile.destroy(effects.halo, 500)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    info.changeLifeBy(-1)
+})
+let myEnemy: Sprite = null
+let myFuel: Sprite = null
+let projectile: Sprite = null
+let statusbar: StatusBarSprite = null
+let mySprite: Sprite = null
+effects.starField.startScreenEffect()
+mySprite = sprites.create(assets.image`bluePlane`, SpriteKind.Player)
+controller.moveSprite(mySprite)
+mySprite.setStayInScreen(true)
+statusbar = statusbars.create(20, 4, StatusBarKind.Energy)
+statusbar.attachToSprite(mySprite, -25, 0)
+info.setScore(0)
+game.onUpdateInterval(5000, function () {
+    myFuel = sprites.createProjectileFromSide(img`
+        . . . . . . b b b b . . . . . . 
+        . . . . . . b 4 4 4 b . . . . . 
+        . . . . . . b b 4 4 4 b . . . . 
+        . . . . . b 4 b b b 4 4 b . . . 
+        . . . . b d 5 5 5 4 b 4 4 b . . 
+        . . . . b 3 2 3 5 5 4 e 4 4 b . 
+        . . . b d 2 2 2 5 7 5 4 e 4 4 e 
+        . . . b 5 3 2 3 5 5 5 5 e e e e 
+        . . b d 7 5 5 5 3 2 3 5 5 e e e 
+        . . b 5 5 5 5 5 2 2 2 5 5 d e e 
+        . b 3 2 3 5 7 5 3 2 3 5 d d e 4 
+        . b 2 2 2 5 5 5 5 5 5 d d e 4 . 
+        b d 3 2 d 5 5 5 d d d 4 4 . . . 
+        b 5 5 5 5 d d 4 4 4 4 . . . . . 
+        4 d d d 4 4 4 . . . . . . . . . 
+        4 4 4 4 . . . . . . . . . . . . 
+        `, 0, 30)
+    myFuel.x = randint(5, 155)
+    myFuel.setKind(SpriteKind.Gas)
+})
+game.onUpdateInterval(1000, function () {
+    myEnemy = sprites.createProjectileFromSide(assets.image`UFO`, 0, 50)
+    myEnemy.x = randint(5, 155)
+    myEnemy.setKind(SpriteKind.Enemy)
+})
+game.onUpdateInterval(300, function () {
+    statusbar.value += -2
+    info.changeScoreBy(0.5)
+})
